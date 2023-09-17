b39abbc2415d937dc5c1875c3f7f091767173027
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-09-17 23:21:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function getFormValue() {
-    
+function getFormvalue(event) {
+    event.preventDefault();
+
+  
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
 
-    
+   
     alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
+
+
+var form = document.getElementById("myForm");
+form.addEventListener("submit", getFormValue);
