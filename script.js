ccc0108d4fbab6a5ffa415d25e876be62e24d120
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-10-06 01:17:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function getFormvalue() {
-	event.preventDefault();
-	var form = document.getElementById("form1");
+// Function to get the values of first and last name and display them using alert
+function getFormValue(event) {
+    event.preventDefault(); 
 
     
-    var firstName = form.elements.fname.value;
-    var lastName = form.elements.lname.value;
-    alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById("lastName");
+
+    
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+
+    
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
+
+
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", getFormValue);
